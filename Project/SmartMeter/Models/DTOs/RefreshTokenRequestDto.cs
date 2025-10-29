a4f41ef098751cc994367c44a978ae028b1c722a
ESET0103 <mantu.kumar@esyasoft.com>
2025-10-29 12:36:25 +0530

    Delete Project/SmartMeter directory

--- Project/SmartMeter/Models/DTOs/RefreshTokenRequestDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SmartMeter.Models.DTOs
-{
-    public class RefreshTokenRequestDto
-    {
-        public long UserId { get; set; }
-        public string RefreshToken { get; set; } = string.Empty;
-    }
-}
